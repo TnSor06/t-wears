1cddf55febb0afc4dabf95ef1cef5580f0ed54bf
Tnsor06 <pra006mod@gmail.com>
2020-05-28 17:54:06 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shoppage.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage}></Route>
-      <Route exact path="/shop" component={ShopPage}></Route>
-    </Switch>
+    <div>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./header.styles.scss";
+import { ReactComponent as Logo } from "../../assets/t-wears-logo.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link to="/" className="logo-container">
+        <Logo className="logo"></Logo>
+      </Link>
+      <div className="options">
+        <NavLink to="/shop" className="option">
+          SHOP
+        </NavLink>
+        <NavLink to="/contact" className="option">
+          CONTACT
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
